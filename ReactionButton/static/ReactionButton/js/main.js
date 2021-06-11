8f0ff54ae0aa60532f08c8eb31d3e245c5d4c0ee
{takashi} <{kotekisaikou@gmail.com}>
2021-06-11 22:41:39 +0900

    [fix]HTMLファイルにonclick要素を追加して並べ直しました。

--- ReactionButton/static/ReactionButton/js/main.js
+++ ReactionButton/static/ReactionButton/js/main.js
@@ -1,19 +1,35 @@
-$(function(){
-    // 最初にゲージを0にして、音声ファイルを読み込み
-    $("#renge").addClass("hee0");
-    var audio = $("#heeAudio")[0];
-    // へぇボタンクリックごとの処理
-    $("label").click(function () {
-      // 押す度に range の value を 1ずつ増加させる
-      num = parseInt($("#hee").val());
-      add = 1;
-      $("#hee").val(num + add);
-      // 音声の再生
-      audio.load();
-      audio.play();
-      // class の付与
-      numC = $("#hee").val();
-      cls = "hee" + numC;
-      $("#renge").removeClass().addClass(cls);
+// const $audio = new Audio();
+// $audio.src = "/sound-of-silence.mp3";
+
+// $audio.play();
+
+window.onload = () => {
+    const se     = document.querySelector('#B1_Uso_audio');
+    document.querySelector("#B1_Uso_img").addEventListener("click", () => {
+        se.play();
+    
+    // const se     = document.querySelector('#R1_Sekaiichi_audio');
+    // document.querySelector("#R1_Sekaiichi_img").addEventListener("click", () => {
+    //     se.play();
     });
-  });
+};
+
+// window.onload = () => {
+//     const se     = document.querySelector('#R1_Sekaiichi_audio');
+//     document.querySelector("#R1_Sekaiichi_img").addEventListener("click", () => {
+//         se.play();
+//     });
+// };
+
+// window.onload = function() {
+//     // ページ読み込みと同時にロード
+//     wa.loadFile("./assets/sample.mp3", function(buffer) {
+//       wa.play(buffer);
+  
+//       // ユーザーイベント
+//       var event = "click";
+//       document.addEventListener(event, function() {
+//         wa.playSilent();
+//       });
+//     });
+//   }
